1d4736f75686b51afe7adde17caba1b5112a1bf2
hsadeghidw <hsadeghi@dwavesys.com>
2020-05-28 16:55:51 -0700

    Separate pages, Add static visualization

--- run.py
+++ run.py
@@ -12,6 +12,26 @@ app.config['SECRET_KEY'] = '2b55241464af362a104880e46b36d2b6'
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def home():
+    return render_template('index.html')
+    # form = OptimizationParameters()
+    # if form.validate_on_submit():
+    #     flash(f'Parameters sumbitted successfully!', category='success')
+    #     ids, graphJSON, success, message, run_time = get_graphs_results(form)
+    #     if success == 0:
+    #         flash(message, category='danger')
+    #     elif success == 1:
+    #         flash(message, category='danger')
+    #         flash(f'Solve time: {run_time:.2f}', category='info')
+    #     else:
+    #         flash(f'Solve time: {run_time:.2f}', category='info')
+    #     return render_template('index.html', form=form, ids=ids, graphJSON=graphJSON)
+    # else:
+    #     ids, graphJSON = get_graphs(form)
+    #     return render_template('index.html', form=form, ids=ids, graphJSON=graphJSON)
+
+
+@app.route('/optimization', methods=['GET', 'POST'])
+def optimization():
     form = OptimizationParameters()
     if form.validate_on_submit():
         flash(f'Parameters sumbitted successfully!', category='success')
@@ -23,10 +43,10 @@ def home():
             flash(f'Solve time: {run_time:.2f}', category='info')
         else:
             flash(f'Solve time: {run_time:.2f}', category='info')
-        return render_template('index.html', form=form, ids=ids, graphJSON=graphJSON)
+        return render_template('optimization.html', form=form, ids=ids, graphJSON=graphJSON)
     else:
         ids, graphJSON = get_graphs(form)
-        return render_template('index.html', form=form, ids=ids, graphJSON=graphJSON)
+        return render_template('optimization.html', form=form, ids=ids, graphJSON=graphJSON)
 
 
 if __name__ == '__main__':
