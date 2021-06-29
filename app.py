f3a08871d6b56f6db9fb3db75f12fbf7b80920bb
Heidi Tong <htong@dwavesys.com>
2021-06-29 14:13:27 -0700

    Use custom css and template for home page

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import streamlit as st
 
 from pages import home, optimization
@@ -5,9 +6,16 @@ from pages import home, optimization
 title = "Resource Distribution Optimization"
 st.set_page_config(page_title=title, layout="wide")
 
+with open("templates/stylesheet.html") as css:
+  stylesheet = css.read()
+
+st.write(stylesheet, unsafe_allow_html=True)
+
 page = st.sidebar.selectbox("Navigation", ["Home", "Optimization"])
 
-st.header(title)
+header = Path("templates/header.html").read_text()
+st.markdown(header, unsafe_allow_html=True)
+
 if page == "Home":
     home.run_page()
 elif page == "Optimization":
