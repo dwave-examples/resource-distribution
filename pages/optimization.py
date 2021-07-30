da383a1b7a6fb3eeb6f3457b85c8d06aed663749
Heidi Tong <htong@dwavesys.com>
2021-07-30 09:35:04 -0700

    Update optimization page and add 'no feasible' runs to results table

--- pages/optimization.py
+++ pages/optimization.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from collections import defaultdict
 from typing import DefaultDict
 
+import jinja2
 import streamlit as st
 from streamlit_folium import folium_static
 import pandas as pd
@@ -25,9 +27,19 @@ def validate_input(form: FormInput) -> bool:
     
     return True
 
-def run_page():
-    """Runs when user visits optimization page, and on any user input."""
-    # Generate sidebar inputs
+def header():
+    """Render 'templates/header.html'."""
+    # Could just do a st.header, but using a template instead so that the header can be shared
+    # between pages (and use the same css).
+    template_dir = Path(__file__).absolute().parent.parent.joinpath('templates')
+    loader = jinja2.FileSystemLoader(template_dir)
+    env = jinja2.Environment(loader=loader)
+    
+    header_html = env.get_template('header.html').render()
+    st.components.v1.html(header_html, height=60, scrolling=False)
+
+def sidebar():
+    """Render sidebar. Returns the user input (button, form)."""
     st.sidebar.markdown("""---""")
     num_hospitals = st.sidebar.number_input("Number of Hospitals", value=12, min_value=2)
     update_button = st.sidebar.button("Update Map", key="update")
@@ -40,43 +52,51 @@ def run_page():
     run_button = st.sidebar.button("Run Optimization", key="run")
     st.sidebar.markdown("""---""")
 
-    form = FormInput(num_hospitals=num_hospitals, 
-                    partition_size=partition_size, 
-                    num_neighbors=num_neighbors, 
-                    dof=dof, 
-                    solver=solver, 
-                    time_limit=time_limit)
+    return run_button, FormInput(num_hospitals=num_hospitals,
+                                 partition_size=partition_size,
+                                 num_neighbors=num_neighbors,
+                                 dof=dof,
+                                 solver=solver,
+                                 time_limit=time_limit)
+
+def run_page():
+    """Runs when user visits optimization page, and on any user input."""
+    header()
+    run_button, form = sidebar()
 
     # Initialize map and results
-    folium_map = get_empty_map(num_hospitals)
-    results_dict = ResultsTable()
+    folium_map = get_empty_map(form.num_hospitals)
+    results_dict = ResultsTable()   # cache containing previous results
 
     # On run, update map and results
     if run_button:
         if validate_input(form):
             figure, result = get_results(form)
 
+            results_dict['# of Hospitals'].append(form.num_hospitals)
+            results_dict['Partition Size'].append(form.partition_size)
+            results_dict['# of Neighbors'].append(form.num_neighbors)
+            results_dict['Dist. Objective Fraction'].append(form.dof)
+            results_dict['Solver'].append(form.solver)
+
             if result:
                 st.sidebar.success("Found solution!")
-                results_dict['# of Hospitals'].append(num_hospitals)
-                results_dict['Partition Size'].append(partition_size)
-                results_dict['# of Neighbors'].append(num_neighbors)
-                results_dict['Dist. Objective Fraction'].append(dof)
-                results_dict['Solver'].append(form.solver)
                 results_dict['Utility'].append(result.total_utility)
                 results_dict['Cost'].append(result.total_cost)
                 results_dict['Energy'].append(result.energy)
                 results_dict['Run Time'].append(result.t)
-                
                 folium_map = figure
             else:
                 st.sidebar.warning("No feasible solution found.")
+                results_dict['Utility'].append('N/A')
+                results_dict['Cost'].append('N/A')
+                results_dict['Energy'].append('N/A')
+                results_dict['Run Time'].append('N/A')
 
     # Display map and results
     folium_static(folium_map, width=map_width, height=map_height)
 
     st.subheader("Results")
-    
     clear_button = st.button("Clear Results")
     if clear_button:
         results_dict.clear()
