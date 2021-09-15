665d9f22bd873a05f324dff6f216884be846f6bf
Heidi Tong <htong@dwavesys.com>
2021-09-15 00:54:58 -0700

    Add utils.py and remove optimization page

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 
-from pages import home, optimization
+from pages import home
 
 title = "Resource Distribution Optimization"
 st.set_page_config(page_title=title, layout="wide")
 
-page = st.sidebar.selectbox("Navigation", ["Home", "Optimization"])
+page = st.sidebar.selectbox("Navigation", ["Home", "Optimization - BQM", "Optimization - CQM"])
 
 if page == "Home":
     home.run_page()
-elif page == "Optimization":
-    optimization.run_page()
